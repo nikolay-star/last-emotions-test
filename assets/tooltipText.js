b426921a098189b489fc15f950aa7c9b3a59f72a
Nikolay Starikov <nickolay-starikov@ya.ru>
2018-10-04 19:52:00 +0300

    add js file for filters

---
+++ assets/tooltipText.js
@@ -0,0 +1,30 @@
+$(document).ready(function () {
+	
+	$('.js-tooltip-login').attr('title', ' Some text here ');
+	
+	$('.js-tooltip-filters').attr('title', ' Some text here ');
+	
+	$('.js-tooltip-filters-dates').attr('title', ' Some text here ');
+	
+	$('.js-tooltip-filters-emotions').attr('title', ' Some text here ');
+	
+	$('.js-tooltip-filters-exp').attr('title', ' Some text here ');
+	
+	$('.js-tooltip-filters-country').attr('title', ' Some text here ');
+	
+	$('.js-tooltip-filters-age').attr('title', ' Some text here ');
+	
+	$('.js-tooltip-bert').attr('title', ' Some text here ');
+	
+	$('.js-tooltip-emotional-journey').attr('title', ' Some text here ');
+	
+	$('.js-tooltip-emotions').attr('title', ' Some text here ');
+	
+	$('.js-tooltip-exp').attr('title', ' Some text here ');
+	
+	$('.js-tooltip-country').attr('title', ' Some text here ');
+	
+	$('.js-tooltip-age').attr('title', ' Some text here ');
+	
+	$('.js-tooltip-gender').attr('title', ' Some text here 2');
+});
